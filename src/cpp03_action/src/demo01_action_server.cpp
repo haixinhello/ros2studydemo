0794c1f41538c58281311612bf63e9b754dcee23
haixin <haixinhello@gmail.com>
2025-07-17 17:24:29 +0800

    feat:Add action communication client

--- src/cpp03_action/src/demo01_action_server.cpp
+++ src/cpp03_action/src/demo01_action_server.cpp
@@ -9,88 +9,87 @@ using std::placeholders::_2;
 class ProgressActionServer: public rclcpp::Node
 {
 public:
-  ProgressActionServer():Node("progress_action_server_node_cpp")
-  {
-    RCLCPP_INFO(this->get_logger(),"action 服务端创建！");
-    server_=rclcpp_action::create_server<Progress>(
-      this,
-      "get_sum",
-      std::bind(&ProgressActionServer::handle_goal,this,_1,_2),
-      std::bind(&ProgressActionServer::handle_cancel,this,_1),
-      std::bind(&ProgressActionServer::handle_accepted,this,_1)
-    );
+  	ProgressActionServer():Node("progress_action_server_node_cpp")
+  	{
+    	RCLCPP_INFO(this->get_logger(),"action 服务端创建！");
+    	server_=rclcpp_action::create_server<Progress>(
+      	this,
+      	"get_sum",
+      	std::bind(&ProgressActionServer::handle_goal,this,_1,_2),
+      	std::bind(&ProgressActionServer::handle_cancel,this,_1),
+      	std::bind(&ProgressActionServer::handle_accepted,this,_1)
+    	);
   }
 private:
-  rclcpp_action::Server<Progress>::SharedPtr server_;
+  	rclcpp_action::Server<Progress>::SharedPtr server_;
 
-  //处理提交的目标值
-  rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid,std::shared_ptr<const Progress::Goal> goal)
-  {
-    (void)uuid;
-    //判断提交的数字是否大于1，是就接收，否则就拒绝
-    if(goal->num<=1)
-    {
-      RCLCPP_INFO(this->get_logger(),"提交的目标值必须大于1！");
-      return rclcpp_action::GoalResponse::REJECT;
-    }
-    RCLCPP_INFO(this->get_logger(),"提交的目标值合法！");
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-  }
+ 	 //处理提交的目标值
+  	rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid,std::shared_ptr<const Progress::Goal> goal)
+  	{
+    	(void)uuid;
+    	//判断提交的数字是否大于1，是就接收，否则就拒绝
+    	if(goal->num<=1)
+    	{
+      		RCLCPP_INFO(this->get_logger(),"提交的目标值必须大于1！");
+      		return rclcpp_action::GoalResponse::REJECT;
+    	}
+    	RCLCPP_INFO(this->get_logger(),"提交的目标值合法！");
+    	return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  	}
 
-  //处理取消请求
-  rclcpp_action::CancelResponse handle_cancel(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
-  {
-    (void)goal_handle;
-    RCLCPP_INFO(this->get_logger(),"接收到任务取消请求！");
-    return rclcpp_action::CancelResponse::ACCEPT;
-  }
+  	//处理取消请求
+ 	 rclcpp_action::CancelResponse handle_cancel(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
+  	{
+		(void)goal_handle;
+    	RCLCPP_INFO(this->get_logger(),"接收到任务取消请求！");
+    	return rclcpp_action::CancelResponse::ACCEPT;
+ 	}
 
   //生成连续反馈与最终响应
-  void execute(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
-  {
-    //生成连续反馈返回给客户端
-    //goal_handle->publish_feedback();
-    int num = goal_handle->get_goal()->num;
-    int sum =0;
-    auto feedback = std::make_shared<Progress::Feedback>();
+  	void execute(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
+  	{
+    	//生成连续反馈返回给客户端
+    	//goal_handle->publish_feedback();
+    	int num = goal_handle->get_goal()->num;
+    	int sum =0;
+    	auto feedback = std::make_shared<Progress::Feedback>();
 
-    rclcpp::Rate rate(1.0);
-    auto result = std::make_shared<Progress::Result>();
-    for (int i = 0; i <= num; i++)
-    {
-      sum += i;
-      double progress = i/(double)num;
-      feedback->progress=progress;
-      goal_handle->publish_feedback(feedback);
-      RCLCPP_INFO(this->get_logger(),"连续反馈中，进度：%.2f",progress);
+    	rclcpp::Rate rate(1.0);
+    	auto result = std::make_shared<Progress::Result>();
+    	for (int i = 0; i <= num; i++)
+    	{
+			sum += i;
+      		double progress = i/(double)num;
+      		feedback->progress=progress;
+      		goal_handle->publish_feedback(feedback);
+      		RCLCPP_INFO(this->get_logger(),"连续反馈中，进度：%.2f",progress);
 
-      if (goal_handle->is_canceling())
-      {
-        result->sum=sum;
-        goal_handle->canceled(result);
-        RCLCPP_INFO(this->get_logger(),"任务被取消了！");
-        return;
-      }
-      rate.sleep();
-      
-    }
+      		if (goal_handle->is_canceling())
+      		{
+        		result->sum=sum;
+        		goal_handle->canceled(result);
+        		RCLCPP_INFO(this->get_logger(),"任务被取消了！");
+        		return;
+      		}
+      		rate.sleep();
+    	}
 
-    //生成最终响应结果
-    //goal_handle->succeed();
-    if (rclcpp::ok())
-    {
-      result->sum=sum;
-      goal_handle->succeed(result);
-      RCLCPP_INFO(this->get_logger(),"最终结果：%d",sum);
-    }
+    	//生成最终响应结果
+    	//goal_handle->succeed();
+    	if (rclcpp::ok())
+    	{
+    		result->sum=sum;
+      		goal_handle->succeed(result);
+     		RCLCPP_INFO(this->get_logger(),"最终结果：%d",sum);
+    	}
     
 
-  }
-  void handle_accepted(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
-  {
-    std::thread(std::bind(&ProgressActionServer::execute,this,goal_handle)).detach();
-    
-  }
+  	}
+	
+  	void handle_accepted(std::shared_ptr<rclcpp_action::ServerGoalHandle<Progress>> goal_handle)
+  	{
+    	std::thread(std::bind(&ProgressActionServer::execute,this,goal_handle)).detach();
+    }
 };
 
 
--- src/cpp03_action/src/demo02_action_client.cpp
+++ src/cpp03_action/src/demo02_action_client.cpp
@@ -1,5 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "base_interfaces_demo/action/progress.hpp"
 
+using base_interfaces_demo::action::Progress;
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+using std::placeholders::_2;
 
 class ProgressActionClient: public rclcpp::Node
 {
@@ -7,14 +13,85 @@ public:
     ProgressActionClient():Node("progress_action_client_node_cpp")
     {
         RCLCPP_INFO(this->get_logger(),"action 客户端创建！");
+		client_=rclcpp_action::create_client<Progress>(this,"get_sum");
     }
+	void send_goal(int num)
+	{
+		if(!client_->wait_for_action_server(10s))
+		{
+			RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"服务端连接失败！");
+			return;
+		}
+
+		auto goal = Progress::Goal();
+		goal.num = num;
+		rclcpp_action::Client<Progress>::SendGoalOptions options;
+		options.goal_response_callback = std::bind(&ProgressActionClient::goal_response_callback,this,_1);
+		options.feedback_callback = std::bind(&ProgressActionClient::feedback_callback,this,_1,_2);
+		options.result_callback = std::bind(&ProgressActionClient::result_callback,this,_1);
+		auto future = client_->async_send_goal(goal,options);
+	}
+
+	void goal_response_callback(rclcpp_action::ClientGoalHandle<Progress>::SharedPtr goal_handle)
+	{
+		if (!goal_handle)
+		{
+			RCLCPP_INFO(this->get_logger(),"目标请求被服务端拒绝！");
+		}
+		else
+		{
+			RCLCPP_INFO(this->get_logger(),"目标处理中！");
+		}
+		
+	}
+
+	void feedback_callback(rclcpp_action::ClientGoalHandle<Progress>::SharedPtr goal_handle,const std::shared_ptr<const Progress::Feedback> feedback)
+	{
+		(void)goal_handle;
+		double progress = feedback->progress;
+		int pro = (int)(progress * 100);
+		RCLCPP_INFO(this->get_logger(),"当前进度：%d%%",pro);
+	}
+
+	void result_callback(const rclcpp_action::ClientGoalHandle<Progress>::WrappedResult & result)
+	{
+		if (result.code == rclcpp_action::ResultCode::SUCCEEDED)
+		{
+			RCLCPP_INFO(this->get_logger(),"最终结果：%d",result.result->sum);
+		}
+		else if(result.code == rclcpp_action::ResultCode::ABORTED)
+		{
+			RCLCPP_INFO(this->get_logger(),"被中断！");
+		}
+		else if(result.code == rclcpp_action::ResultCode::CANCELED)
+		{
+			RCLCPP_INFO(this->get_logger(),"被取消！");
+		}
+		else
+		{
+			RCLCPP_INFO(this->get_logger(),"未知异常！");
+		}
+		
+		
+	}
+
+private:
+	rclcpp_action::Client<Progress>::SharedPtr client_;
 };
 
 
 int main(int argc, char const *argv[])
 {
+    if (argc!=2)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"请传入一个整型数据！");
+        return 1;
+    }
+    
     rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<ProgressActionClient>());
+	auto node=std::make_shared<ProgressActionClient>();
+	node->send_goal(atoi(argv[1]));
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
